4e53e110a455608bd904211b2dd484ea5471f148
Isaque <contato@isaqueoliva.com>
2020-04-02 21:00:52 -0300

    Isaque_FP

---
+++ Isaque_FP/Isaque_FP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Isaque_FP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TesteUnitario/TesteUnitario/TesteUnitario/TesteUnitario/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TesteUnitario
+{
+    class Program
+    {   
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public int Somar(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        public float DividirFloat(float value1, float value2)
+        {
+            return value1 / value2;    
+        }
+
+        public int DividirInt(int value1, int value2)
+        {
+            return value1 / value2;
+        }
+
+        /*
+        // Arrange
+        int n1 = 10, n2 = 20;
+        Calculator calculator = new Calculator();
+        // Act
+        int result = calculator.Sum(n1, n2);
+        // Assert
+        Assert.Equal(30, result);
+        */
+
+    }
+}
