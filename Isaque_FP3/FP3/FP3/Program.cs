d47ca5c07b7a3bfc1b96125771dcc0dfed3a4e00
Isaque Oliveira <contato.isaqueoliveira.com>
2020-04-29 11:46:48 -0300

    FP3 Ex

---
+++ Isaque_FP3/FP3/FP3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FP3
+{
+    /*
+    Map: (F<T>, (T -> R)) -> F<R>
+    Map pega um functor F<T> e uma função f: T -> R e retorna um F<R> cujo valor interno é o resultado de aplicar a função f para o valor interno de F<T>.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Option<int> number = 2;
+            Func<int, int> plusThree = x => x + 3;
+
+            Option<int> result = number.Map(plusThree);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class FP
+    {
+        public static None None => None.Value;
+
+        public static Option<T> Some<T>(T value) => new Some<T>(value);
+
+
+    }
+    public struct None
+    {
+        public static readonly None Value = new None();
+    }
+
+    public struct Some<T>
+    {
+        public T Value { get; }
+        public Some(T value)
+        {
+            if (value == null) throw new ArgumentException();
+            Value = value;
+        }
+    }
+
+    public struct Option<T>
+    {
+        readonly T Value;
+        readonly bool IsSome;
+
+        private Option(T value)
+        {
+            Value = value;
+            IsSome = true;
+        }
+        public static implicit operator Option<T>(None _) => new Option<T>();
+        public static implicit operator Option<T>(Some<T> some) => new Option<T>(some.Value);
+        public static implicit operator Option<T>(T value) => value == null ? FP.None : FP.Some(value);
+
+        public TResult Match<TResult>(Func<TResult> None, Func<T, TResult> Some) => IsSome ? Some(Value) : None();
+        public Option<TResult> Map<TResult>(Func<T, TResult> function)
+        {
+            Option<TResult> result = function(Value);
+            return result;
+        }
+
+        public override string ToString() => IsSome ? $"{Value}" : "Option.None";
+    }
+}
---
+++ Isaque_FP3/FP3/FP3/README_FP3.txt
@@ -0,0 +1,8 @@
+Isaque Oliveira
+
+Map 
+
+1. Aplica uma função em um valor encapsulado 
+2. Retira o valor encapsulado do seu contexto, aplica a função passada como parâmetro e retorna o resultado encapsulado.
+3. O outro programador deve chamar a função que é do tipo de retorno Option<T> em uma variável do mesmo tipo já que é genérica, passando como parâmetros a variável para se aplicar a função e a função.
+4. Deve ser usada quando se quer aplicar uma função específica à um valor.
